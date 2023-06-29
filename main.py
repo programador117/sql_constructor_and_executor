9df91f0390a21c12dec3721269b5528c585180e6
Yanier Jauregui Gonzalez <yanierjaureguigonzalez@gmail.com>
2023-06-29 12:20:08 +0200

    Main: Execute all sql process (from load to save)

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from sql_constructor_by_table import construct_sql_queries_for_all_cases
 
-if __name__ == "__main__":
-    pass
+if __name__ == '__main__':
+    construct_sql_queries_for_all_cases()
