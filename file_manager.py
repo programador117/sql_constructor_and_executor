59bbee43bdc4fcbb53033e460387b2789c4a53fb
Yanier Jauregui Gonzalez <yanierjaureguigonzalez@gmail.com>
2023-06-29 15:03:39 +0200

    File Management: Read .txt files, connection data and save results to excel

--- file_manager.py
+++ file_manager.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 
 def find_files(folder_path, file_name, file_extension):
     files = []
@@ -11,6 +12,25 @@ def find_files(folder_path, file_name, file_extension):
                     files.append(file_path)
     return files
 
+def read_from_file(file_path):
+    with open(file_path, 'r') as f:
+        content = f.read()
+    return content
+
+def read_connection_data(file_path):
+    with open(file_path, 'r') as f:
+        data = f.read().splitlines()
+    return {
+        'host': data[0],
+        'database': data[1],
+        'user': data[2],
+        'password': data[3]
+    }
+
 def save_to_file(file_path, content):
     with open(file_path, 'w') as f:
         f.write(content)
+
+def save_result_to_excel(data, case_name):
+    file_path = os.path.join('resultado_consultas', f'F130_{case_name}.xlsx')
+    data.to_excel(file_path, index=False)
