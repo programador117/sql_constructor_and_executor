110ab35a39db9a6697e281e61e91b37d828ccc24
Yanier Jauregui Gonzalez <yanierjaureguigonzalez@gmail.com>
2023-06-29 15:04:39 +0200

    Main: Execute all queries

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from sql_constructor_by_table import construct_sql_queries_for_all_cases
+from query_constructor_by_table import construct_sql_queries_for_all_cases
+from all_queries_executor import execute_sql_queries_for_all_cases
 
 if __name__ == '__main__':
     construct_sql_queries_for_all_cases()
+    execute_sql_queries_for_all_cases()
